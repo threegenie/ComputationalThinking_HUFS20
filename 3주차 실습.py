5f094150e0073739af16d46a39e5bce620230fb1
Sejin <63702924+threegenie@users.noreply.github.com>
2020-09-15 14:21:51 +0900

    Add files via upload

---
+++ 3주차 실습.py
@@ -0,0 +1,59 @@
+#컴퓨팅사고 3주차 실습 1
+
+a = int(input())
+b = float(input())
+
+print(a+b, type(a+b))
+
+c = int(input())
+d = int(input())
+
+print(c+d, type(c+d))
+
+
+e = float(input())
+f = float(input())
+
+print(e+f, type(e+f))
+
+
+#컴퓨팅사고 3주차 실습 2
+
+print('왓츤 아줌마는 밤낮으로 나를 보기만 하면 하는 소리가 "허클베리, 발을 그런 데다 얹는 게 아니야."라든지, "허클베리, 그렇게 덜거럭거리지 말고 똑바로 앉아라." 하는 거였습니다.\n\n그러고는 금방 "허클베리, 그렇게 하품을 하거나 기지개를 켜는 게 아니야. 왜 얌전하게 굴지 않는 거지?"하고 잔소리를 해대는 겁니다. 그러고는 버릇없이 굴면 가게 된다는 그 놈의 지옥 이야기를 하나도 빼놓지 않고 해주는 바람에 나는 정말로 거기에 가봤으면 좋겠다고 했습니다.')
+
+
+#컴퓨팅사고 3주차 실습 3
+
+num1 = 17.5
+num2 = 10
+cpx1 = 3 + 3j
+cpx2 = 1 - 2j
+str1 = "Hello"
+str2 = "goorm"
+
+print(num1+num2,type(num1+num2))
+print(str1+str2,type(str1+str2))
+print(num1+cpx1,type(num1+cpx1))
+print(num1*num1,type(num1*num1))
+print(num1/num2,type(num1/num2))
+print(num1//num2,type(num1//num2))
+print(num1%num2,type(num1%num2))
+print(cpx1/cpx2,type(cpx1/cpx2))
+
+
+#컴퓨팅사고 3주차 마무리 과제
+
+a = 10
+b = 3
+c = 1.5
+
+print(a+b)
+print(a-b)
+print(a//b)
+print(a%b)
+
+b *= 2
+print(b)
+
+a *= c
+print(a)
